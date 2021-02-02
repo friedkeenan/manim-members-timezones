dcb4d457fd62e3291b42a2f943a8b4e9a9290374
naveen521kk <naveen@syrusdark.website>
2021-02-02 20:09:15 +0000

    Deploying to gh-pages from  @ 568c241cf2c009fed01385667883b01e52d140cb 🚀

---
+++ precache-manifest.d5f9c09489ba6c3b9850e26b659350d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736fadbe75ae045aa066d405dfb56553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff9e4b5ca09bd9e3276a",
+    "url": "/static/css/main.807c9f8b.chunk.css"
+  },
+  {
+    "revision": "f45e96ab8948f0749b23",
+    "url": "/static/js/2.424e6b4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.424e6b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff9e4b5ca09bd9e3276a",
+    "url": "/static/js/main.138c0671.chunk.js"
+  },
+  {
+    "revision": "616ea038efeec10a7ddd",
+    "url": "/static/js/runtime-main.1676ba94.js"
+  }
+]);
